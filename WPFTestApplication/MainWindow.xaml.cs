6a75b0e72a0a699281a2cc654454c88490e3a53d
nebsdsamwu <nebsd.sam.wu@gmail.com>
2016-08-03 17:05:49 -0700

    first commit

---
+++ WPFTestApplication/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// my Map 
+using Microsoft.Maps.MapControl.WPF;
+
+namespace BingMapWPFAppication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            myMap.Focus();
+            myMap.Mode = new RoadMode();
+        }
+    }
+
+    public class TextInputToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Always test MultiValueConverter inputs for non-null 
+            // (to avoid crash bugs for views in the designer) 
+            if (values[0] is bool && values[1] is bool)
+            {
+                bool hasText = !(bool)values[0];
+                bool hasFocus = (bool)values[1];
+                if (hasFocus || hasText)
+                    return Visibility.Collapsed;
+            }
+            return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
---
+++ WPFTestApplication/SwitchMapViews.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// for BingMapWPFAppication
+using System.Globalization;
+using Microsoft.Maps.MapControl.WPF;
+using Microsoft.Maps.MapControl.WPF.Design;
+
+
+namespace BingMapWPFAppication
+{
+    /// <summary>
+    /// Interaction logic for SwitchMapViews.xaml
+    /// </summary>
+    public partial class SwitchMapViews : Window
+    {
+        LocationConverter locConvertor = new LocationConverter();
+
+        public SwitchMapViews()
+        {
+            InitializeComponent();
+            myMapViews.Focus();
+            myMapViews.ViewChangeOnFrame += new EventHandler<MapEventArgs>(viewMap_ViewChangeOnFrame);
+        }
+
+        private void viewMap_ViewChangeOnFrame(object sender, MapEventArgs e)
+        {
+            Map map = sender as Map;
+
+            if (map != null)
+            {
+                Location mapCenter = map.Center;
+                txtLatitude.Text = string.Format(CultureInfo.InvariantCulture, "{0:F5}", mapCenter.Latitude);
+                txtLongitude.Text = string.Format(CultureInfo.InvariantCulture, "{0:F5}", mapCenter.Longitude);
+            }
+        }
+
+        private void ChangeMapView_Click(object sender, RoutedEventArgs e)
+        {
+            string[] tagInfo = ((Button)sender).Tag.ToString().Split(' ');
+            Location center = (Location)locConvertor.ConvertFrom(tagInfo[0]);
+            double zoom = System.Convert.ToDouble(tagInfo[1]);
+            myMapViews.SetView(center, zoom);
+        }
+
+        private void AnimationLevel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)(((ComboBox)sender).SelectedItem);
+            string v = cbi.Content as string;
+            if (!string.IsNullOrEmpty(v) && myMapViews != null)
+            {
+                AnimationLevel newLevel = (AnimationLevel)Enum.Parse(typeof(AnimationLevel), v, true);
+                myMapViews.AnimationLevel = newLevel;
+            }
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (TargetZip.Trim() == "")
+            {
+                MessageBox.Show("Please enter a ZIP code");
+            }
+            else
+            {
+                MessageBox.Show(TargetZip);
+            }
+        }
+
+        public string TargetZip
+        {
+            get { return txtTargetZip.Text; }
+        }
+    }
+}

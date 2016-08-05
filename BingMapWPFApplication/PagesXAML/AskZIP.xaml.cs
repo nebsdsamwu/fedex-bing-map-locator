bf8ef881764669f47f4598289864b91bbacb88b7
nebsdsamwu <sam.s.wu@newegg.com>
2016-08-05 10:18:17 -0700

    add ask input page

---
+++ BingMapWPFApplication/PagesXAML/AskZIP.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BingMapWPFApplication.PagesXAML
+{
+    /// <summary>
+    /// Interaction logic for AskZIP.xaml
+    /// </summary>
+    public partial class AskZIP : Window
+    {
+        public AskZIP()
+        {
+            InitializeComponent();
+        }
+    }
+}
